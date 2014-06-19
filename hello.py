08ebc91852fb5ff506b22a7e877ed3c2c1bd2da9
Lyzi Diamond <lyzidiamond@gmail.com>
2014-06-18 19:48:22 -0700

    Move HTML to template

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import json
 import StringIO
 import shapely
 from shapely.geometry import shape, mapping
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, render_template
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['js', 'json', 'geojson'])
@@ -50,35 +50,7 @@ def operation():
 
       return send_file(strIO, attachment_filename=centroids_filename, as_attachment=True)
 
-  return '''
-  <title>Upload New File</title>
-  <body>
-    <div id="header">
-      <h1>Centroids!</h1>
-      <h2>This application reads a valid geojson file and returns a valid geojson file of centroids.</h2>
-    </div>
-    <div id="notes">
-      <p>In the resulting file:</p>
-      <ul>
-        <li>All properties are retained in the output file.</li>
-        <li>Polygon features become points that represent the Polygon's centroid.</li>
-        <li>LineString features become points that represent the middle of the LineString.</li>
-        <li>Point features do not change.</li>
-        <li>I have no idea how this handles MultiPolygons.</li>
-      </ul>
-    </div>
-    <div id="form">
-      <form action="" method=post enctype=multipart/form-data>
-        <p><input type=file name=file>
-          <input type=submit value=Make centroids>
-        </p>
-      </form>
-    </div>
-    <div id="footer">
-      <p>This is a Flask application that uses the Shapely Python library. It was made by <a href="http://twitter.com/lyzidiamond">Lyzi Diamond</a>. <a href="http://github.com/lyzidiamond/centroids">It is on GitHub.</a></p>
-    </div>
-  </body>
-  '''
+  return render_template('index.html')
 
 @app.route('/centroids', methods=['POST'])
 def api_centroids():
